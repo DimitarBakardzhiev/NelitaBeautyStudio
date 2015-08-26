4f352b1301fb61b2e252f1681d2022a4f91cd3b2
Rossoner40 <dimitar.bakardzhiev94@gmail.com>
2015-08-26 16:29:10 +0300

    Default error page has been added for 404 status codes. A news model has been added.

---
+++ NelitaBeautyStudio.Models/News.cs
@@ -0,0 +1,20 @@
+﻿namespace NelitaBeautyStudio.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class News
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [Required]
+        public DateTime CreatedOn { get; set; }
+    }
+}
---
+++ NelitaBeautyStudio.Web/Controllers/ErrorController.cs
@@ -0,0 +1,20 @@
+﻿namespace NelitaBeautyStudio.Web.Controllers
+{
+    using System.Web.Mvc;
+
+    using NelitaBeautyStudio.Data.Unit_of_Work;
+
+    public class ErrorController : BaseController
+    {
+        public ErrorController(IApplicationData data)
+            : base(data)
+        {
+        }
+
+        public ViewResult NotFound()
+        {
+            Response.StatusCode = 404;
+            return this.View();
+        }
+    }
+}
