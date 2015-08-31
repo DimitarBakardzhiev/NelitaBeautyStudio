af302e5b5f1b02d9a16349c6f54e6883d95c4ab9
Rossoner40 <dimitar.bakardzhiev94@gmail.com>
2015-08-31 20:55:19 +0300

    A page viewing all news with server-side paging has been added. All view titles have been edited.

--- NelitaBeautyStudio.Common/GlobalConstants.cs
+++ NelitaBeautyStudio.Common/GlobalConstants.cs
@@ -22,6 +22,7 @@
 
         public const int TextFieldLengthMax = 75;
         public const int TextFieldLengthMin = 6;
+        public const int NewsIndexPageSize = 10;
 
         public const double PriceMinimum = 0.0;
         public const double PriceMaximum = 500.0;
--- NelitaBeautyStudio.Web/Controllers/NewsController.cs
+++ NelitaBeautyStudio.Web/Controllers/NewsController.cs
@@ -4,8 +4,10 @@
     using System.Linq;
     using System.Web.Mvc;
 
+    using NelitaBeautyStudio.Common;
     using NelitaBeautyStudio.Data.Unit_of_Work;
 
+    [Authorize(Roles = GlobalConstants.AdminRole)]
     public class NewsController : BaseController
     {
         public NewsController(IApplicationData data)
@@ -25,6 +27,34 @@
             latestNews = latestNews.Where(n => n.CreatedOn > DateTime.Now.AddMonths(-1)).ToList();
 
             return this.PartialView(latestNews);
+        }
+
+        //[AllowAnonymous]
+        //public ActionResult Index()
+        //{
+        //    var allNews = this.Data.News.All().OrderByDescending(n => n.CreatedOn).ToList();
+        //    var latestTen = allNews.Take(GlobalConstants.NewsIndexPageSize);
+
+        //    return this.View(latestTen);
+        //}
+
+        [AllowAnonymous]
+        public ActionResult Index(int page = 1)
+        {
+            var allNews = this.Data.News.All().OrderByDescending(n => n.CreatedOn).ToList();
+            var currentPage = allNews.Skip((page - 1)* GlobalConstants.NewsIndexPageSize).Take(GlobalConstants.NewsIndexPageSize);
+
+            this.ViewData["currentPage"] = page;
+            var pagesCount = allNews.Count / GlobalConstants.NewsIndexPageSize;
+
+            if (allNews.Count % GlobalConstants.NewsIndexPageSize != 0)
+            {
+                pagesCount++;
+            }
+
+            this.ViewData["pagesCount"] = pagesCount;
+
+            return this.View(currentPage);
         }
     }
 }
