5b638c64811fe94b0fa2778306a63b4407a82d4e
Rossoner40 <dimitar.bakardzhiev94@gmail.com>
2015-09-01 18:28:43 +0300

    Tests has been merged to master.

---
+++ NelitaBeautyStudio.Tests/ControllerTests/ContactsControllerTests.cs
@@ -0,0 +1,51 @@
+﻿namespace NelitaBeautyStudio.Tests.ControllerTests
+{
+    using System.Linq;
+    using System.Web.Mvc;
+
+    using NelitaBeautyStudio.Tests.ControllerTests.Data;
+    using NelitaBeautyStudio.Web.Controllers;
+    using NUnit.Framework;
+    using NelitaBeautyStudio.Models;
+    using System.Collections.Generic;
+    using NelitaBeautyStudio.Web.ViewModels;
+
+    [TestFixture]
+    public class ContactsControllerTests
+    {
+        private ContactsController controller { get; set; }
+
+        [TestFixtureSetUp]
+        public void Init()
+        {
+            controller = new ContactsController(new FakeApplicationData());
+        }
+
+        [Test]
+        public void IndexShouldReturnCorrectView()
+        {
+            var result = controller.Index() as ViewResult;
+
+            Assert.AreEqual("Index", result.ViewName);
+        }
+
+        [Test]
+        public void IndexShouldReturnModel()
+        {
+            var result = controller.Index() as ViewResult;
+
+            Assert.IsNotNull(result.Model);
+        }
+
+        [Test]
+        public void IndexShouldReturnSortedModel()
+        {
+            var result = controller.Index() as ViewResult;
+            var model = (List<Contact>)result.Model;
+            var sortedModel = model.OrderByDescending(c => c.Priority)
+                .ThenBy(c => c.Type);
+
+            Assert.AreEqual(model, sortedModel);
+        }
+    }
+}
