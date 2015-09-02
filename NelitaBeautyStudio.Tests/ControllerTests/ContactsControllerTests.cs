4ccdfc59803a1a498846784fd9e1cfd5000de366
Rossoner40 <dimitar.bakardzhiev94@gmail.com>
2015-09-02 16:24:38 +0300

    StyleCop errors have been fixed.

--- NelitaBeautyStudio.Tests/ControllerTests/ContactsControllerTests.cs
+++ NelitaBeautyStudio.Tests/ControllerTests/ContactsControllerTests.cs
@@ -1,30 +1,30 @@
 ﻿namespace NelitaBeautyStudio.Tests.ControllerTests
 {
+    using System.Collections.Generic;
     using System.Linq;
     using System.Web.Mvc;
 
+    using NelitaBeautyStudio.Models;
     using NelitaBeautyStudio.Tests.ControllerTests.Data;
     using NelitaBeautyStudio.Web.Controllers;
+    
     using NUnit.Framework;
-    using NelitaBeautyStudio.Models;
-    using System.Collections.Generic;
-    using NelitaBeautyStudio.Web.ViewModels;
 
     [TestFixture]
     public class ContactsControllerTests
     {
-        private ContactsController controller { get; set; }
+        private ContactsController Controller { get; set; }
 
         [TestFixtureSetUp]
         public void Init()
         {
-            controller = new ContactsController(new FakeApplicationData());
+            this.Controller = new ContactsController(new FakeApplicationData());
         }
 
         [Test]
         public void IndexShouldReturnCorrectView()
         {
-            var result = controller.Index() as ViewResult;
+            var result = this.Controller.Index() as ViewResult;
 
             Assert.AreEqual("Index", result.ViewName);
         }
@@ -32,7 +32,7 @@
         [Test]
         public void IndexShouldReturnModel()
         {
-            var result = controller.Index() as ViewResult;
+            var result = this.Controller.Index() as ViewResult;
 
             Assert.IsNotNull(result.Model);
         }
@@ -40,7 +40,7 @@
         [Test]
         public void IndexShouldReturnSortedModel()
         {
-            var result = controller.Index() as ViewResult;
+            var result = this.Controller.Index() as ViewResult;
             var model = (List<Contact>)result.Model;
             var sortedModel = model.OrderByDescending(c => c.Priority)
                 .ThenBy(c => c.Type);
@@ -48,19 +48,19 @@
             Assert.AreEqual(model, sortedModel);
         }
 
-        //[Test]
-        //public void CreateValidContactShouldRedirectToIndex()
-        //{
-        //    var contact = new ContactViewModel() 
-        //    {
-        //        Priority = Priority.Low, 
-        //        Type = "telefona na pesho", 
-        //        Value = "1432567536245"
-        //    };
+        ////[Test]
+        ////public void CreateValidContactShouldRedirectToIndex()
+        ////{
+        ////    var contact = new ContactViewModel() 
+        ////    {
+        ////        Priority = Priority.Low, 
+        ////        Type = "telefona na pesho", 
+        ////        Value = "1432567536245"
+        ////    };
 
-        //    var result = (RedirectToRouteResult)controller.Create(contact);
+        ////    var result = (RedirectToRouteResult)controller.Create(contact);
 
-        //    Assert.AreEqual("Index", "");
-        //}
+        ////    Assert.AreEqual("Index", "");
+        ////}
     }
 }
