b9ecfced54813e17b3c00f792911514a89106bd0
Dimitar Bakardzhiev <dimitar.bakardzhiev94@gmail.com>
2015-10-23 20:35:00 +0300

    Possible solution to a database connection issue.

--- NelitaBeautyStudio.Web/Controllers/HomeController.cs
+++ NelitaBeautyStudio.Web/Controllers/HomeController.cs
@@ -3,7 +3,7 @@
     using System.Web.Mvc;
 
     using NelitaBeautyStudio.Data.Unit_of_Work;
-
+    
     public class HomeController : BaseController
     {
         public HomeController(IApplicationData data)
